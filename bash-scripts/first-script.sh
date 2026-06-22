1e6af42ea161fb262581458abc87c32e09e292fb
Tayo <adiotayoibrahim@gmail.com>
2026-06-22 01:42:43 +0100

    Add first bash script

--- bash-scripts/first-script.sh
+++ bash-scripts/first-script.sh
@@ -5,3 +5,14 @@ echo "Welcome to Bash scripting"
 date
 pwd
 ls
+
+Why Devops engineers use bash scripts
+checking servers
+checking disk space
+checking memory
+testing websites
+creating backups
+searching logs
+creating folders
+deploying applications
+checking network connectivity
